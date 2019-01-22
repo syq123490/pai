d0f149a5c43339671bdc5f055f7f6453ba86c625
yuye@microsoft.com <yuye@microsoft.com>
2019-01-22 17:53:44 +0800

    dynamic police to update machine list.

--- src/rest-server/src/config/paiConfig.js
+++ src/rest-server/src/config/paiConfig.js
@@ -21,30 +21,39 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const logger = require('./logger');
 
-let paiMachineList = [];
-try {
-    paiMachineList = yaml.safeLoad(fs.readFileSync('/pai-cluster-config/cluster-configuration.yaml', 'utf8'))['machine-list'];
-} catch (err) {
-    paiMachineList = [];
-    logger.info('Unable to load machine list from cluster-configuration.');
-    logger.info(err.stack);
+function updateMachineList() {
+    let paiMachineList = [];
+    try {
+        paiMachineList = yaml.safeLoad(fs.readFileSync('/pai-cluster-config/cluster-configuration.yaml', 'utf8'))['machine-list'];
+    } catch (err) {
+        paiMachineList = [];
+        logger.info('Unable to load machine list from cluster-configuration.');
+        logger.info(err.stack);
+    }
+
+    let paiConfigData = {
+        machineList: paiMachineList,
+    };
+
+
+    // define the schema for pai configuration
+    const paiConfigSchema = Joi.object().keys({
+        machineList: Joi.array(),
+    }).required();
+
+
+    const {error, value} = Joi.validate(paiConfigData, paiConfigSchema);
+    if (error) {
+        throw new Error(`config error\n${error}`);
+    }
+    paiConfigData = value;
+
+    module.exports = paiConfigData;
+    logger.info("Machine list for az-rdma is updated.")
 }
 
-let paiConfigData = {
-    machineList: paiMachineList,
-};
+updateMachineList();
+// Execute every 10 mins
+setInterval(updateMachineList, 600000);
 
 
-// define the schema for pai configuration
-const paiConfigSchema = Joi.object().keys({
-    machineList: Joi.array(),
-}).required();
-
-
-const {error, value} = Joi.validate(paiConfigData, paiConfigSchema);
-if (error) {
-    throw new Error(`config error\n${error}`);
-}
-paiConfigData = value;
-
-module.exports = paiConfigData;
